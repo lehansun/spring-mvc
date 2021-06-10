97f21592a5567178ebab5ecb768a3ee2899ba7bb
Aliaksei Vazdusevich <avazdusevich@gmail.com>
2021-06-10 22:13:48 +0300

    Lesson-20: Model example + How to use Tymeleaf example

--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -1,19 +1,21 @@
 package ru.alishev.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
         if (name != null && surname != null) {
-            System.out.println("Hello, " + name + " " + surname);
+//            System.out.println("Hello, " + name + " " + surname);
+            model.addAttribute("message", "Hello, " + name + " " + surname);
         }
         return "first/hello";
     }
