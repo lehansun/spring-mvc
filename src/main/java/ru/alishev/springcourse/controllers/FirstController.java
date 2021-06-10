191d79912d95806c8d61d9ec5be0b6d6b0595ba9
Aliaksei Vazdusevich <avazdusevich@gmail.com>
2021-06-10 21:37:01 +0300

    Lesson19-1: HttpServletRequest example

--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -3,11 +3,18 @@ package ru.alishev.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        if (!name.isEmpty() && !surname.isEmpty()) {
+            System.out.println("Hello, " + name + " " + surname);
+        }
         return "first/hello";
     }
 
