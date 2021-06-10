12530c54918ac788cc85b0e5656b43692bc4a901
Aliaksei Vazdusevich <avazdusevich@gmail.com>
2021-06-10 12:25:39 +0300

    ADD JAVA-configuration

--- src/main/java/ru/alishev/springcourse/config/MySpringMvcDispatcherServletInitializer.java
+++ src/main/java/ru/alishev/springcourse/config/MySpringMvcDispatcherServletInitializer.java
@@ -1,2 +1,24 @@
-package ru.alishev.springcourse.config;public class MySpringMvcDispatcherServletInitializer {
+package ru.alishev.springcourse.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+//  Данный класс заменяет файл web.xml
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+//    Данный метод не используется
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+//    Указываем, какие классы необходимо использовать для конфигурации проекта
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+//    Все запросы от пользователя будут перенаправлены на DispatcherServlet
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
 }
