f152f7c25f8f55d0e9466b657fa84d5e53f4e08f
Aliaksei Vazdusevich <avazdusevich@gmail.com>
2021-06-11 15:33:12 +0300

    Lesson-25: Add data validation

--- src/main/java/ru/alishev/springcourse/model/Person.java
+++ src/main/java/ru/alishev/springcourse/model/Person.java
@@ -5,13 +5,24 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.*;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Person {
     private int id;
+
+    @NotEmpty(message = "Name should not be empty!")
+    @Size(min = 2, max = 30, message =  "Name should be between 2 and 30 characters")
     private String name;
+
+    @Min(value = 0, message = "Age should be grater than 0!")
+    @Max(value = 120, message = "Age should be less than 120!")
     private Integer age;
+
+    @NotEmpty(message = "Email should not be empty!")
+    @Email(message = "Email should be valid")
     private String email;
 }
